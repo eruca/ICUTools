5e4b19e6d814277ffb636f192cdc3b16280be75d
eruca <nickwill1984@126.com>
2020-10-11 20:02:00 +0800

    添加项目文件。

---
+++ ICUTools/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ICUTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_temp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_hr = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_bp = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_bp);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_hr);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_temp);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(651, 175);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "生命体征";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_result);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 193);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(651, 98);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "体温:";
+            // 
+            // textBox_temp
+            // 
+            this.textBox_temp.Location = new System.Drawing.Point(79, 23);
+            this.textBox_temp.Multiline = true;
+            this.textBox_temp.Name = "textBox_temp";
+            this.textBox_temp.Size = new System.Drawing.Size(556, 40);
+            this.textBox_temp.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "心率:";
+            // 
+            // textBox_hr
+            // 
+            this.textBox_hr.Location = new System.Drawing.Point(79, 69);
+            this.textBox_hr.Multiline = true;
+            this.textBox_hr.Name = "textBox_hr";
+            this.textBox_hr.Size = new System.Drawing.Size(556, 40);
+            this.textBox_hr.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "血压:";
+            // 
+            // textBox_bp
+            // 
+            this.textBox_bp.Location = new System.Drawing.Point(79, 115);
+            this.textBox_bp.Multiline = true;
+            this.textBox_bp.Name = "textBox_bp";
+            this.textBox_bp.Size = new System.Drawing.Size(556, 40);
+            this.textBox_bp.TabIndex = 3;
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(79, 26);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(556, 56);
+            this.textBox_result.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(547, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "清空";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "结果:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(664, 348);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ICU工具";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_bp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_hr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_temp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ICUTools/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ICUTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private IList<double> convert(string text, string title)
+        {
+            if (text == string.Empty)
+            {
+                return null;
+            }
+
+            var values = new List<double>();
+            foreach (var s in Regex.Split(text, "\\s+"))
+            {
+                double d;
+                if (!double.TryParse(s, out d))
+                {
+                    MessageBox.Show(string.Format("{0}错误，必须是数字", title));
+                    return null;
+                }
+                values.Add(d);
+            }
+            return values;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            // 体温转化
+            var temps = convert(textBox_temp.Text, "温度");
+            if (temps != null)
+            {
+                stringBuilder.Append(string.Format("体温：{0}~{1}℃", temps.Min(), temps.Max()));
+            }
+
+            // 心率转化
+            var hrs = convert(textBox_hr.Text, "心率");
+            if (hrs != null)
+            {
+                if (stringBuilder.Length > 0)
+                {
+                    stringBuilder.Append('，');
+                }
+                stringBuilder.Append(string.Format("心率：{0}~{1}次/分", hrs.Min(), hrs.Max()));
+            }
+
+            string bp = textBox_bp.Text;
+            if (bp != string.Empty)
+            {
+                List<double> systolic = new List<double>();
+                List<double> diastolic = new List<double>();
+
+                foreach (var single in Regex.Split(bp, "\\s+"))
+                {
+                    try
+                    {
+                        var parts = single.Split('/').Select(v => double.Parse(v)).ToArray();
+                        if (parts.Count() != 2)
+                        {
+                            MessageBox.Show("血压输入的格式不对，必须都是 100/60 格式");
+                            return;
+                        }
+                        systolic.Add(parts[0]);
+                        diastolic.Add(parts[1]);
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("血压输入格式错误，必须是140/90");
+                        return;
+                    }
+                }
+
+                if (stringBuilder.Length > 0)
+                {
+                    stringBuilder.Append('，');
+                }
+                stringBuilder.Append(string.Format("血压: {0}~{1}/{2}~{3}mmHg", systolic.Min(), systolic.Max(), diastolic.Min(), diastolic.Max()));
+            }
+
+            textBox_result.Text = stringBuilder.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox_temp.Clear();
+            textBox_hr.Clear();
+            textBox_bp.Clear();
+            textBox_result.Clear();
+        }
+    }
+}
