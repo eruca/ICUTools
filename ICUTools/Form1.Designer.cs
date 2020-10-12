7cb8c0a231b72e6aabaadd1e12e1b6b91f88f694
eruca <nickwill1984@126.com>
2020-10-12 14:02:46 +0800

    增加SpO2等

--- ICUTools/Form1.Designer.cs
+++ ICUTools/Form1.Designer.cs
@@ -29,108 +29,273 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox_temp = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox_hr = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.textBox_bp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_spo2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_hr = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_rr = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_temp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.textBox_result = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.numericUpDown4);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
             this.groupBox1.Controls.Add(this.textBox_bp);
             this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_spo2);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label9);
             this.groupBox1.Controls.Add(this.textBox_hr);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.textBox_rr);
+            this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.textBox_temp);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(651, 175);
+            this.groupBox1.Size = new System.Drawing.Size(651, 217);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "生命体征";
             // 
-            // groupBox2
+            // numericUpDown4
             // 
-            this.groupBox2.Controls.Add(this.textBox_result);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Location = new System.Drawing.Point(12, 193);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(651, 98);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
+            this.numericUpDown4.Location = new System.Drawing.Point(533, 171);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(101, 23);
+            this.numericUpDown4.TabIndex = 4;
             // 
-            // label1
+            // numericUpDown3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "体温:";
+            this.numericUpDown3.Location = new System.Drawing.Point(376, 171);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(98, 23);
+            this.numericUpDown3.TabIndex = 4;
             // 
-            // textBox_temp
+            // numericUpDown2
             // 
-            this.textBox_temp.Location = new System.Drawing.Point(79, 23);
-            this.textBox_temp.Multiline = true;
-            this.textBox_temp.Name = "textBox_temp";
-            this.textBox_temp.Size = new System.Drawing.Size(556, 40);
-            this.textBox_temp.TabIndex = 1;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(219, 171);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(110, 23);
+            this.numericUpDown2.TabIndex = 4;
             // 
-            // label2
+            // numericUpDown1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 69);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 20);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "心率:";
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(61, 171);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(99, 23);
+            this.numericUpDown1.TabIndex = 4;
             // 
-            // textBox_hr
+            // textBox_bp
             // 
-            this.textBox_hr.Location = new System.Drawing.Point(79, 69);
-            this.textBox_hr.Multiline = true;
-            this.textBox_hr.Name = "textBox_hr";
-            this.textBox_hr.Size = new System.Drawing.Size(556, 40);
-            this.textBox_hr.TabIndex = 2;
+            this.textBox_bp.Location = new System.Drawing.Point(61, 115);
+            this.textBox_bp.Multiline = true;
+            this.textBox_bp.Name = "textBox_bp";
+            this.textBox_bp.Size = new System.Drawing.Size(573, 40);
+            this.textBox_bp.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(21, 115);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.Size = new System.Drawing.Size(35, 17);
             this.label3.TabIndex = 0;
             this.label3.Text = "血压:";
             // 
-            // textBox_bp
+            // textBox_spo2
             // 
-            this.textBox_bp.Location = new System.Drawing.Point(79, 115);
-            this.textBox_bp.Multiline = true;
-            this.textBox_bp.Name = "textBox_bp";
-            this.textBox_bp.Size = new System.Drawing.Size(556, 40);
-            this.textBox_bp.TabIndex = 3;
+            this.textBox_spo2.Location = new System.Drawing.Point(373, 69);
+            this.textBox_spo2.Multiline = true;
+            this.textBox_spo2.Name = "textBox_spo2";
+            this.textBox_spo2.Size = new System.Drawing.Size(262, 40);
+            this.textBox_spo2.TabIndex = 2;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(480, 171);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 17);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "总出量:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(332, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "SPO2:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(335, 171);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 17);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "大便:";
+            // 
+            // textBox_hr
+            // 
+            this.textBox_hr.Location = new System.Drawing.Point(374, 23);
+            this.textBox_hr.Multiline = true;
+            this.textBox_hr.Name = "textBox_hr";
+            this.textBox_hr.Size = new System.Drawing.Size(262, 40);
+            this.textBox_hr.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(166, 171);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 17);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "尿量:";
+            // 
+            // textBox_rr
+            // 
+            this.textBox_rr.Location = new System.Drawing.Point(61, 69);
+            this.textBox_rr.Multiline = true;
+            this.textBox_rr.Name = "textBox_rr";
+            this.textBox_rr.Size = new System.Drawing.Size(260, 40);
+            this.textBox_rr.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "总入量:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "心率:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "呼吸:";
+            // 
+            // textBox_temp
+            // 
+            this.textBox_temp.Location = new System.Drawing.Point(61, 23);
+            this.textBox_temp.Multiline = true;
+            this.textBox_temp.Name = "textBox_temp";
+            this.textBox_temp.Size = new System.Drawing.Size(260, 40);
+            this.textBox_temp.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "体温:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_result);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 235);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(651, 98);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
             // 
             // textBox_result
             // 
-            this.textBox_result.Location = new System.Drawing.Point(79, 26);
+            this.textBox_result.Location = new System.Drawing.Point(61, 26);
             this.textBox_result.Multiline = true;
             this.textBox_result.Name = "textBox_result";
-            this.textBox_result.Size = new System.Drawing.Size(556, 56);
+            this.textBox_result.Size = new System.Drawing.Size(574, 56);
             this.textBox_result.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "结果:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(440, 304);
+            this.button1.Location = new System.Drawing.Point(440, 350);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 32);
             this.button1.TabIndex = 5;
@@ -140,7 +305,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(547, 304);
+            this.button2.Location = new System.Drawing.Point(547, 350);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 32);
             this.button2.TabIndex = 6;
@@ -148,21 +313,12 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 26);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 20);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "结果:";
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(664, 348);
+            this.ClientSize = new System.Drawing.Size(667, 394);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
@@ -174,6 +330,10 @@
             this.Text = "ICU工具";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -194,6 +354,18 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_spo2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_rr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- ICUTools/Form1.cs
+++ ICUTools/Form1.cs
@@ -41,24 +41,38 @@ namespace ICUTools
             StringBuilder stringBuilder = new StringBuilder();
 
             // 体温转化
-            var temps = convert(textBox_temp.Text, "温度");
+            var temps = convert(textBox_temp.Text.Trim(), "温度");
             if (temps != null)
             {
                 stringBuilder.Append(string.Format("体温：{0}~{1}℃", temps.Min(), temps.Max()));
             }
 
             // 心率转化
-            var hrs = convert(textBox_hr.Text, "心率");
+            var hrs = convert(textBox_hr.Text.Trim(), "心率");
             if (hrs != null)
             {
-                if (stringBuilder.Length > 0)
-                {
-                    stringBuilder.Append('，');
-                }
+                CheckStringBuilder(stringBuilder);
                 stringBuilder.Append(string.Format("心率：{0}~{1}次/分", hrs.Min(), hrs.Max()));
             }
 
-            string bp = textBox_bp.Text;
+            // 呼吸
+            var rr = convert(textBox_rr.Text.Trim(), "呼吸");
+            if (rr != null)
+            {
+                CheckStringBuilder(stringBuilder);
+                stringBuilder.Append(string.Format("呼吸次数:{0}~{1}次/分", rr.Min(), rr.Max()));
+            }
+
+            // 呼吸
+            var spo2 = convert(textBox_spo2.Text.Trim(), "SPO2");
+            if (rr != null)
+            {
+                CheckStringBuilder(stringBuilder);
+                stringBuilder.Append(string.Format("SPO2:{0}~{1}%", rr.Min(), rr.Max()));
+            }
+
+            // 血压
+            string bp = textBox_bp.Text.Trim();
             if (bp != string.Empty)
             {
                 List<double> systolic = new List<double>();
@@ -84,14 +98,49 @@ namespace ICUTools
                     }
                 }
 
-                if (stringBuilder.Length > 0)
-                {
-                    stringBuilder.Append('，');
-                }
+                CheckStringBuilder(stringBuilder);
                 stringBuilder.Append(string.Format("血压: {0}~{1}/{2}~{3}mmHg", systolic.Min(), systolic.Max(), diastolic.Min(), diastolic.Max()));
             }
 
+            if (numericUpDown1.Value > 0)
+            {
+                CheckStringBuilder(stringBuilder);
+                stringBuilder.Append(string.Format("总入量:{0}ml", numericUpDown1.Value));
+            }
+
+            if (numericUpDown2.Value > 0)
+            {
+                CheckStringBuilder(stringBuilder);
+                stringBuilder.Append(string.Format("尿量:{0}ml", numericUpDown2.Value));
+            }
+
+            if (numericUpDown3.Value > 0)
+            {
+                CheckStringBuilder(stringBuilder);
+                stringBuilder.Append(string.Format("大便:{0}ml", numericUpDown3.Value));
+            }
+
+            if (numericUpDown4.Value > 0)
+            {
+                CheckStringBuilder(stringBuilder);
+                stringBuilder.Append(string.Format("总出量:{0}ml", numericUpDown4.Value));
+            }
+
+            if (numericUpDown2.Value + numericUpDown3.Value > numericUpDown4.Value)
+            {
+                MessageBox.Show("总出量必须大于尿量+大便");
+                return;
+            }
+
             textBox_result.Text = stringBuilder.ToString();
+        }
+
+        private void CheckStringBuilder(StringBuilder sb)
+        {
+            if (sb.Length > 0)
+            {
+                sb.Append('，');
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
